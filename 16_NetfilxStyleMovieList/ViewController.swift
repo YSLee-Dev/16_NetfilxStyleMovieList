8d75e918f40e7b7d60abf58a9ffe8d94329be308
YSLee-Dev <ysleedev01@gmail.com>
2022-06-13 19:16:03 +0900

    프로젝트 생성

---
+++ 16_NetfilxStyleMovieList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  16_NetfilxStyleMovieList
+//
+//  Created by 이윤수 on 2022/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
