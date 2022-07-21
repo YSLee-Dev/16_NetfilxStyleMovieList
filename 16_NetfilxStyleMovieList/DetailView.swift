bab2048b77bc5f0a527e37cd735e3f3680a508e5
YSLee-Dev <ysleedev01@gmail.com>
2022-07-21 19:09:34 +0900

    디자인 변경 (Swift UI)

--- 16_NetfilxStyleMovieList/DetailView.swift
+++ 16_NetfilxStyleMovieList/DetailView.swift
@@ -8,24 +8,34 @@
 import SwiftUI
 
 struct DetailView: View {
-    @State var item : Item?
+    var item : Item?
     
     var body: some View {
-        VStack{
-            if let item = item{
-                Image(uiImage: item.image)
-                    .aspectRatio(contentMode: .fit)
-                
-                Text(item.description)
-                    .font(.caption)
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.center)
-                    .padding()
-            }else{
-                Color.white
+        ZStack{
+            Color.black.edgesIgnoringSafeArea(.all)
+            ZStack(alignment: .bottom){
+                if let item = item{
+                    Image(uiImage: item.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200)
+                    
+                    Text(item.description)
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .foregroundColor(.primary)
+                        .background(Color.primary
+                                        .colorInvert()
+                                        .opacity(0.75))
+                }else{
+                    Color.white
+                }
             }
         }
     }
+    
 }
 
 struct DetailView_Previews: PreviewProvider {
